30e3b6c241f58bce8c049c07888eec594d8c1046
Anton Sochnev <an.sochnev@yandex.ru>
2019-05-21 01:08:14 +0300

    Enable async; need debug

--- input_mux.h
+++ input_mux.h
@@ -6,16 +6,15 @@ class InputMux
     std::stringstream m_data;
     char              m_buf[64];
 
+    async::handle_t m_shared_context;
+    async::handle_t m_context;
+
     int m_nesting_level;
 
-    async::handle_t m_shared;
-    async::handle_t m_private;
-
 public:
-    InputMux(async::handle_t h) : m_shared(h)
-    {
-        m_private = async::connect(28);
-    }
+    InputMux(async::handle_t shared, async::handle_t context) 
+        : m_shared_context(shared), m_context(context), m_nesting_level(0) 
+    {}
 
     void receive(const char* data, std::size_t n)
     {
@@ -25,22 +24,26 @@ public:
         while(m_data.getline(m_buf, 64))
         {
             s = m_buf;
+            
             if (s == "{") {
                 ++m_nesting_level;
             } 
             else if (s == "}") {
                 --m_nesting_level;
             } 
-            else if (m_nesting_level == 0) {
-                async::receive(m_shared, s.c_str(), s.size());
+
+            s.append("\n");
+
+            if (m_nesting_level == 0) {
+                async::receive(m_shared_context, s.c_str(), s.size());
             } 
             else {
-                async::receive(m_private, s.c_str(), s.size());
+                async::receive(m_context, s.c_str(), s.size());
             }
         }
-    }
 
-    ~InputMux() {
-        async::disconnect(m_private);
+        if (!m_data) {
+            m_data.clear();
+        }
     }
 };
--- server.cpp
+++ server.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
 #include <sstream>
+#include <memory>
 
 #include <boost/asio.hpp>
 #include <libasync/async.h>
 
-class Session
+#include "input_mux.h"
+
+class Session : public std::enable_shared_from_this<Session>
 {
     boost::asio::ip::tcp::socket m_socket;
     char m_buf[1024];
 
+    async::handle_t m_shared_context;
+    async::handle_t m_private_context;
+
+    InputMux *m_mux;
+
 public:
-    Session(boost::asio::ip::tcp::socket sock)
-        : m_socket(std::move(sock))
+    Session(boost::asio::ip::tcp::socket sock, 
+            async::handle_t shared, 
+            int bulk)
+        : m_socket(std::move(sock)), m_shared_context(shared)
+
+    {
+        async::handle_t m_private_context = async::connect(bulk);
+        m_mux = new InputMux(m_shared_context, m_private_context);
+    }
+
+    ~Session() 
+    {
+        m_socket.close();
+        async::disconnect(m_private_context);
+        delete m_mux;
+    }
+
+    void start() 
     {
         do_read();
     }
@@ -19,46 +43,58 @@ public:
 private:
     void do_read()
     {
+        auto self(shared_from_this());
         m_socket.async_read_some(boost::asio::buffer(m_buf),
-            [this](const boost::system::error_code& err, std::size_t n)
+            [this, self](const boost::system::error_code& err, std::size_t n)
             {
-                if (err) {
-                    std::cout << err.message() << std::endl;
-                    return;
-                }               
-                do_read();
+                if (!err) {
+                    m_mux->receive(m_buf, n);
+                    do_read();
+                }
             });
     }
 };
 
 class Server 
 {
-    boost::asio::ip::tcp::acceptor  m_acceptor;
-    boost::asio::ip::tcp::socket    m_server_socket;
+    boost::asio::ip::tcp::acceptor m_acceptor;
+    boost::asio::ip::tcp::socket   m_socket;
 
-    Session *m_client_session;
+    async::handle_t m_shared_context;
+    int m_bulk;
 
 public:
-    Server(boost::asio::io_service& service, short port) 
+    Server(boost::asio::io_service& service, short port, int bulk) 
         : 
         m_acceptor(service, 
                      boost::asio::ip::tcp::endpoint(
                          boost::asio::ip::tcp::v4(), port)), 
-        m_server_socket(service),
-        m_client_session(nullptr)
+        m_socket(service),
+        m_bulk(bulk)
     {
+        async::init();
+        m_shared_context = async::connect(bulk);
         do_accept();
+    }
+
+    ~Server()
+    {
+        m_socket.close();
+        async::disconnect(m_shared_context);
+        async::close();
     }
 
 private:
     void do_accept()
     {
-        m_acceptor.async_accept(m_server_socket, 
+        m_acceptor.async_accept(m_socket, 
             [this](const boost::system::error_code& err) 
             {
-                std::cout << "accepted" << std::endl;
-                std::cout << err.message() << std::endl;
-                m_client_session = new Session(std::move(m_server_socket));
+                if (!err) {
+                    std::make_shared<Session>(std::move(m_socket), 
+                                              m_shared_context,
+                                              m_bulk)->start();
+                }
                 this->do_accept();
             });
     }
@@ -66,9 +102,8 @@ private:
 
 int main(int argc, char* argv[]) 
 {
-
     boost::asio::io_service service;
-    Server server(service, 9000);
+    Server server(service, 9000, 3);
     service.run();
 
     return 0;
